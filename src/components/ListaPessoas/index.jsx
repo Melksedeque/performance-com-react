b7cdaf76840b6b6424ef27721eaf9ecdbbec8365
Melksedeque <freelancer@melksedeque.com.br>
2025-03-26 18:59:13 -0300

    Classes Bulma para estilização da lista e do botão

--- src/components/ListaPessoas/index.jsx
+++ src/components/ListaPessoas/index.jsx
@@ -7,12 +7,13 @@ export default function ListaPessoas() {
     <>
       <ul>
         {pessoas.map((pessoa) => (
-          <li key={pessoa}>
+          <li key={pessoa} className="box mb-3">
             <Pessoa nome={pessoa} />
           </li>
         ))}
       </ul>
       <button
+        className="button is-primary is-outlined mt-5"
         onClick={() =>
           setPessoas((listaAnterior) => [...listaAnterior, "Melk"])
         }
