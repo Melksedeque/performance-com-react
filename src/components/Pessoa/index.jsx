78e769d8c1dc960782db9f3497b64ec1c4a9afda
Melksedeque <freelancer@melksedeque.com.br>
2025-03-26 19:05:01 -0300

    useMemo para renderizar somente componentes novos

--- src/components/Pessoa/index.jsx
+++ src/components/Pessoa/index.jsx
@@ -1,6 +1,16 @@
-function Pessoa({ nome }) {
-  console.log(`Pessoa ${nome} renderizou`);
-  return <p>{nome}</p>;
+import { useMemo } from "react";
+
+function Pessoa({ nome, deletar, id }) {
+  useMemo(() => {
+    console.log(`Pessoa ${nome} foi atualizado`);
+  }, [nome]);
+
+  return (
+    <li key={nome} className="box mb-3">
+      <p>{nome}</p>
+      <button onClick={() => deletar(id)}>Me delete!</button>
+    </li>
+  );
 }
 
 export default Pessoa;
