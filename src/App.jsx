bf506dc1321124722fd691c1ab9d368139735aca
Melksedeque <freelancer@melksedeque.com.br>
2025-03-26 18:57:01 -0300

    Inclusão de classes CSS do Bulma; Inclusão de heading tags

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import ListaPessoas from "./components/ListaPessoas";
 
 function App() {
   return (
-    <div>
+    <div className="has-text-centered">
+      <h1 className="title">Lista de Pessoas</h1>
+      <h2 className="subtitle">Clique no botão para adicionar uma pessoa</h2>
+      <hr />
       <ListaPessoas />
     </div>
   );
